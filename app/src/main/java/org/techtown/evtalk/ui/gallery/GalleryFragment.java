97c51caf5f3788c61223a3ede44c7a2daf8f1697
haesung97 <ajdehd12@gmail.com>
2021-04-02 14:35:35 +0900

    GalleryFragment에 임시로 로그아웃 버튼 생성

--- app/src/main/java/org/techtown/evtalk/ui/gallery/GalleryFragment.java
+++ app/src/main/java/org/techtown/evtalk/ui/gallery/GalleryFragment.java
@@ -1,20 +1,24 @@
 package org.techtown.evtalk.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+
+import org.techtown.evtalk.LoginActivity;
 import org.techtown.evtalk.R;
 
 public class GalleryFragment extends Fragment {
+    private Button btn_login_out;
 
     private GalleryViewModel galleryViewModel;
 
@@ -23,13 +27,22 @@ public class GalleryFragment extends Fragment {
         galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        btn_login_out = (Button)root.findViewById(R.id.btn_login_out);
+        btn_login_out.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                UserManagement.getInstance().requestLogout(new LogoutResponseCallback() {
+                    @Override
+                    public void onCompleteLogout() {
+                        getActivity().finish();
+                        startActivity(intent);
+                    }
+                });
             }
         });
+
         return root;
     }
 }
