0ef880b97c00244018afe12c86a5f4c4f354b28d
tbqlsmm <qldtn8@naver.com>
2021-04-09 00:10:09 +0900

    충전소 기본정보 받아오기

---
+++ app/src/main/java/org/techtown/evtalk/ChargingStation.java
@@ -0,0 +1,94 @@
+package org.techtown.evtalk;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ChargingStation {
+    @SerializedName("id")
+    private String id;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("address")
+    private String address;
+
+    @SerializedName("lat")
+    private float lat;
+
+    @SerializedName("lng")
+    private float lng;
+
+    @SerializedName("useTime")
+    private String useTime;
+
+    @SerializedName("park")
+    private String park;
+
+    @SerializedName("note")
+    private String note;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public void setLng(float lng) {
+        this.lng = lng;
+    }
+
+    public void setUseTime(String useTime) {
+        this.useTime = useTime;
+    }
+
+    public void setPark(String park) {
+        this.park = park;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public float getLng() {
+        return lng;
+    }
+
+    public String getUseTime() {
+        return useTime;
+    }
+
+    public String getPark() {
+        return park;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+}
