bc6282706739067f3378b2c2e9349253eb7ebd8b
haesung97 <ajdehd12@gmail.com>
2021-04-09 21:04:24 +0900

    충전소 마커 찍기 (전체 마커를 가상으로 찍은 후 핸드폰 화면 가운데를 기준으로 반경 3km이내 충전소들만 마커를 동적으로 표시하도록 한다)

--- app/src/main/java/org/techtown/evtalk/MainActivity.java
+++ app/src/main/java/org/techtown/evtalk/MainActivity.java
@@ -24,6 +24,9 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.CameraUpdate;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
@@ -31,12 +34,15 @@ import com.naver.maps.map.NaverMapOptions;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.LocationOverlay;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.OverlayImage;
 import com.naver.maps.map.util.FusedLocationSource;
 import com.naver.maps.map.widget.LocationButtonView;
 import com.pedro.library.AutoPermissions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -121,6 +127,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 //                Toast.LENGTH_SHORT).show();
 
         AutoPermissions.Companion.loadAllPermissions(this, 101);
+
+
+
     }
 
     @Override // home 에서 뒤로가기 두번 클릭 시 종료됩니다
@@ -196,6 +205,70 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // 현재 위치 표시
         LocationOverlay locationOverlay = naverMap.getLocationOverlay();
         locationOverlay.setVisible(true);
+
+        // 카메라 초기 위치 설정
+        LatLng initialPosition = new LatLng(37.506855, 127.066242);
+        CameraUpdate cameraUpdate = CameraUpdate.scrollTo(initialPosition);
+        naverMap.moveCamera(cameraUpdate);
+
+        // 마커들 위치 정의
+        markersPosition = new Vector<LatLng>();
+        for (int i = 0; i < chargingStation.size(); i++) {
+            markersPosition.add(new LatLng(chargingStation.get(i).getLat(),chargingStation.get(i).getLng()));
+        }
+
+        // 카메라 이동 되면 호출 되는 이벤트
+        naverMap.addOnCameraChangeListener(new NaverMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(int reason, boolean animated) {
+                freeActiveMarkers();
+                // 정의된 마커위치들중 가시거리 내에있는것들만 마커 생성
+                LatLng currentPosition = getCurrentPosition(naverMap);
+                for (LatLng markerPosition: markersPosition) {
+                    if (!withinSightMarker(currentPosition, markerPosition))
+                        continue;
+                    Marker marker = new Marker();
+                    marker.setIconPerspectiveEnabled(true); // 원근감 표시
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.ic_baseline_location_on_24));
+                    marker.setPosition(markerPosition); // 마커 위치
+                    marker.setMap(naverMap);
+                    activeMarkers.add(marker);
+                }
+            }
+        });
+    }
+
+    // 마커 정보 저장시킬 변수들 선언
+    private Vector<LatLng> markersPosition;
+    private Vector<Marker> activeMarkers;
+
+    // 현재 카메라가 보고있는 위치
+    public LatLng getCurrentPosition(NaverMap naverMap) {
+        CameraPosition cameraPosition = naverMap.getCameraPosition();
+        return new LatLng(cameraPosition.target.latitude, cameraPosition.target.longitude);
+    }
+
+    // 선택한 마커의 위치가 가시거리(카메라가 보고있는 위치 반경 3km 내)에 있는지 확인
+    public final static double REFERANCE_LAT = 1 / 109.958489129649955;
+    public final static double REFERANCE_LNG = 1 / 88.74;
+    public final static double REFERANCE_LAT_X3 = 3 / 109.958489129649955;
+    public final static double REFERANCE_LNG_X3 = 3 / 88.74;
+    public boolean withinSightMarker(LatLng currentPosition, LatLng markerPosition) {
+        boolean withinSightMarkerLat = Math.abs(currentPosition.latitude - markerPosition.latitude) <= REFERANCE_LAT_X3;
+        boolean withinSightMarkerLng = Math.abs(currentPosition.longitude - markerPosition.longitude) <= REFERANCE_LNG_X3;
+        return withinSightMarkerLat && withinSightMarkerLng;
+    }
+
+    // 지도상에 표시되고있는 마커들 지도에서 삭제
+    private void freeActiveMarkers() {
+        if (activeMarkers == null) {
+            activeMarkers = new Vector<Marker>();
+            return;
+        }
+        for (Marker activeMarker: activeMarkers) {
+            activeMarker.setMap(null);
+        }
+        activeMarkers = new Vector<Marker>();
     }
 
     //메인 엑티비티 실행 시 DB에서 유저 정보 받아오기
